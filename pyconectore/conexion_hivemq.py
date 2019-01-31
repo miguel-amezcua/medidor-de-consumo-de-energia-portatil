6f93240940163606efcb0a3e529b47497d719e4e
miguel-amezcua <43039566+miguel-amezcua@users.noreply.github.com>
2019-01-31 11:32:33 -0600

    Update conexion_hivemq.py

--- pyconectore/conexion_hivemq.py
+++ pyconectore/conexion_hivemq.py
@@ -1,31 +1,21 @@
 import time
-
 import paho.mqtt.client as mqtt
 import main_
 
-a = main_.objetivo
+a = main_.mqtt_()
 
 
 # funcion de conexion con servidor mqtt
 def on_connect(client, userdata, flags, rc):
     print("codigo funcionando:" + str(rc))  # topic entrada
-
-
 def on_message(client, userdata, msg):
     print(msg.topic + ' ' + str(msg.payload))
-
-
 def on_publish(client, userdata, mid):  # create function for callback
     print("data published mid=", mid, "\n")
-
-
 def on_disconnect(client, userdata, rc):
     print("client disconnected ok")
-
-
 def on_subscribe(client, userdata, mid, granted_qos):  # create function for callback
     print("subscribed with qos", granted_qos, "\n")
-
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -35,15 +25,13 @@ client.on_publish = on_publish  # assign function to callback
 client.on_subscribe = on_subscribe  # assign function to callback
 # client.username_pw_set =conexion().build1('usuario', 'passw')
 
-
 client.username_pw_set("UMC_Jalisco", "U_ctq9$a")
-# client.connect(conexion().build1('dns','puerto','60'))
-client.connect("sener.ciateq.net.mx", 4000, 60)
-client.subscribe("test enable")  # esta linea de codigo hace funcionar el on_message
+client.connect("sener.ciateq.net.mx", 4001, 60)
+client.subscribe("test/#")  # esta linea de codigo hace funcionar el on_message
 client.loop_start()
 time.sleep(1)
 while True:
-    client.publish("test/",a)
+    client.publish("test/", a)
     time.sleep(5)
 client.loop_stop()
 client.disconnect()
